0accb717275961725668f41902d66b88b5c03128
sydarsky <sydarsky@gmail.com>
2025-12-08 21:14:17 +0200

    nginx init

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
+import socket
 
-app = FastAPI()
+app = FastAPI(
+    root_path="/api"
+)
+
+@app.get("/info")
+async def get_backend():
+    return {"backend": socket.gethostname()}
